f62262cfce31b0951f2659aadb67e5b8b6662dce
Bernat Ferragut <elbernat@gmail.com>
2017-08-07 15:26:11 -0400

    Polishing Seconds code

--- 2-cssclock/main.js
+++ 2-cssclock/main.js
@@ -1,10 +1,12 @@
-// Clock
 
+// Selection
+let secondsHand = document.querySelector('.hand');
+
+// Moving clock Hands => seconds
 function moveHand() {
     const now = new Date();
     let seconds = now.getSeconds();
     let degrees = (seconds/60) * 360;
-    let secondsHand = document.querySelector('.hand');
     secondsHand.style.transform =`rotate(${degrees}deg) `;
 }
 // calling moveHand every second
